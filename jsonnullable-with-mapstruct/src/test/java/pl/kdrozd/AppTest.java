54e7b03cc244b779675d3bf937cb522d63d4c4c0
Konrad Drozd <drozdkonrad@gmail.com>
2021-01-31 14:10:07 +0100

    Create base project

---
+++ jsonnullable-with-mapstruct/src/test/java/pl/kdrozd/AppTest.java
@@ -0,0 +1,14 @@
+package pl.kdrozd;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
